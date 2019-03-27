7e46ffc24eea1b969f84e0f347202c3d83e367a3
陈禹东 <chenyudong@s-MacBook-ea.local>
2019-03-27 20:31:25 +0800

    Better for evaluation and testing.

--- LR_IRLS.py
+++ LR_IRLS.py
@@ -4,7 +4,6 @@ data_path = './a9a/'
 train_file = data_path + 'a9a'
 test_file = data_path + 'a9a.t'
 feature_num = 123
-lam = 1
 
 
 def read_data_from_file(filename):
@@ -32,25 +31,41 @@ def sigmoid(z):
     return ex / (1 + ex)
 
 
-def get_accuracy(X, y, w):
+def get_evaluations(X, y, w):
     mu = sigmoid(X@w)
     mu[mu < 0.5] = 0
     mu[mu >= 0.5] = 1
-    tmp = np.abs(mu - y)
-    acc = 1 - np.sum(tmp) / y.shape[0]
-    return acc
+    sub = mu - y
+    accuracy = 1 - np.sum(np.abs(sub)) / y.shape[0]
+    TP = np.sum(mu * y)
+    FP = np.sum(sub[sub == 1])
+    FN = np.abs(np.sum(sub[sub == -1]))
+    precision = TP / (TP + FP)
+    recall = TP / (TP + FN)
+    f1_score = 2 * precision * recall / (precision + recall)
+    return accuracy, precision, recall, f1_score
 
 
 def L2_norm(w):
     return np.linalg.norm(w)
 
 
-def IRLS(X, y, maxiter=10, break_thres=0.001):
+def IRLS(X, y, lam, maxiter=10, break_thres=0.001):
+    """
+
+    :param X: training features X
+    :param y: training labels y
+    :param lam: lambda for l2-norm penalty of w
+    :param maxiter:
+    :param break_thres: break the IRLS when || w_{t+1} - w_t ||_1 < break_thres
+    :return:
+    """
     N = y.shape[0]
     R = np.diag(np.repeat(1, N))
     z = np.linalg.inv(R)@y
     H = X.T@R@X + lam * np.eye(feature_num)
-    w = np.linalg.pinv(H)@(X.T@R@z)  # 为什么要这样初始化呢？
+    # w = np.linalg.pinv(H)@(X.T@R@z)  # 为什么要这样初始化呢？
+    w = np.random.normal(0, 0.01, feature_num)
     for it in range(maxiter):
         Xw = X@w
         mu = sigmoid(Xw)
@@ -63,18 +78,22 @@ def IRLS(X, y, maxiter=10, break_thres=0.001):
         if np.sum(np.abs(w_next - w)) < break_thres:
             return w_next
         else:
-            print("Thres =", np.sum(np.abs(w_next - w)))
+            # print("Thres =", np.sum(np.abs(w_next - w)))
             w = w_next
-            acc = get_accuracy(X, y, w)
-            print("Iter =", it, ", accuracy =", acc, ", l2_norm =", L2_norm(w))
+            eva = get_evaluations(X, y, w)
+            print("Iter =", it, ", accuracy =", eva[0], ", l2_norm =", L2_norm(w))
+    return w
 
 
 if __name__ == '__main__':
     train_X, train_y = read_data_from_file(train_file)
     test_X, test_y = read_data_from_file(test_file)
     print("y.shape =", train_y.shape)
-    w_final = IRLS(train_X, train_y)
-
-    print("Test accuracy =", get_accuracy(test_X, test_y, w_final), ", l2_norm =", L2_norm(w_final))
+    for lam in [0, 1e-4, 1e-3, 1e-2, 1e-1, 1, 1e1, 1e2, 1e3]:
+    # for lam in [0.1]:
+        w_final = IRLS(train_X, train_y, lam)
+        eva = get_evaluations(test_X, test_y, w_final)
+        print("When lambda = %f test accuracy = %f, precision = %f, recall = %f, f1_score = %f, l2_norm = %f"
+              % (lam, eva[0], eva[1], eva[2], eva[3], L2_norm(w_final)))
 
 
